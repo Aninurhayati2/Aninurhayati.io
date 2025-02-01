ee40f94f0f51d1bb7791ecf775037230eaf99b08
Aninurhayati2 <ani00nurhayati@gmail.com>
2025-02-01 12:25:57 +0700

    first commit

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
 <div class="row justify-content-center">
 <div class="col-md-4 mb-3">
 <div class="card" >
-  <img src="poto1.jpg" class="card-img-top" alt="projects1">
+  <img src="img/poto1.jpg" class="card-img-top" alt="projects1">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
